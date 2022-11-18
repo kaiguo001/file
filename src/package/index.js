b1cafa82fc8c36e72148c331efd8c2ab9b831657
xukai <xukai@nandasoft-its.com>
2022-11-18 09:44:14 +0800

    新增拖动验证码组件

--- src/package/index.js
+++ src/package/index.js
@@ -1,10 +1,10 @@
 // 引用组件
 import ndButton from '@/package/button/index.vue'
-import ndVideo from '@/package/Jessibuca/index.vue'
+import ndVerifycode from '@/package/verificationCode/index.vue'
 
 
 //组件都写在这个数组中方便管理
-const componentList = [ndButton, ndVideo]
+const componentList = [ndButton, ndVerifycode]
 
 // 批量注册
 const install = (Vue) => {
